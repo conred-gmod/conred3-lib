19a00f8363c48063d59330e13fe205881147896d
stpM64 <sssstepa452@yandex.ru>
2026-06-17 01:25:29 +0300

    CR.Class.{MakeConstructable, MakeDestructable}: добавил хуки, для конструктора добавил возможность, чтобы после инициализации объект оставался невалидным

--- lua/conred/lib/class/new_delete_sh.lua
+++ lua/conred/lib/class/new_delete_sh.lua
@@ -3,12 +3,16 @@ local Class = CR.Class
 --- mixin CR.Class.Constructable
 --
 --- .__isvalid -- false by-default, true during and after call of :Init or :New 
+--- .__init_makes_valid = true -- if true, after :Init or :New is called, the object is made valid
 --
 --- :Construct() static -> object(self) -- construct the object without initializing it
 --- :New(ctor_params: ...) static -> object(self) -- construct the object and initialize it
 --
 --- :Init(ctor_params) -- initialize the object (used when you need to delay init from construction, use :New otherwise)
 --- :OnInit(ctor_params: ...) optional
+--
+--- hook CR.Class.PreInit(obj: object(CR.Class.Constructable), ctor_params: ...)
+--- hook CR.Class.PostInit(obj: object(CR.Class.Constructable), ctor_params: ...)
 
 local function class_construct(self)
     local inst = {
@@ -23,18 +27,23 @@ local function class_init(self, ...)
     if IsValid(self) then
         CR.Error("Can't init ",self,": it is already valid (likely already initialized)")
     end
-    
-    self.__isvalid = true
+
+    hook.Run("CR.Class.PreInit", self, ...)
 
     if inst.OnInit ~= nil then
         inst:OnInit(...)
     end
 
+    if self.__init_makes_valid then
+        self.__isvalid = true
+    end
+
+    hook.Run("CR.Class.PostInit", self, ...)
 end
 
 local function class_new(self, ...)
     local inst = class_construct(self)
-    class_init(self, ...)
+    self:Init(...)
 
     return inst
 end
@@ -43,12 +52,14 @@ end
 --- meta: metatable(CR.Class.Base)
 function Class.MakeConstructable(meta)
     meta.__isvalid = false
+    meta.__init_makes_valid = true
 
     meta.Construct = class_construct
     meta.Init = class_init
     meta.New = class_new
 end
 
+------------------------------------------------------
 
 --- mixin CR.Class.Deletable
 --
@@ -56,17 +67,24 @@ end
 --
 --- :OnDelete(del_params: ...) optional -- the destructor/delete handler (called from delete function if it exists)
 --- :Delete(del_params: ...) -- the delete function (can't be called twice)
+--
+--- hook CR.Class.PreDelete(obj: object(CR.Class.Deletable), del_params: ...)
+--- hook CR.Class.PostDelete(obj: object(CR.Class.Deletable), del_params: ...)
 
 local function class_delete(self, ...)
     if not IsValid(self) then
         CR.Error("Can't delete ",self,": it is invalid (likely it is already deleted or was not created (= attempt to delete a metatable))")
     end
 
+    hook.Run("CR.Class.PreDelete", self, ...)
+
     if self.OnDelete ~= nil then
         self:OnDelete(...)
     end
 
     self.__isvalid = false
+
+    hook.Run("CR.Class.PostDelete", self, ...)
 end
 
 --- Adds CR.Class.Deletable mixin to `meta`
