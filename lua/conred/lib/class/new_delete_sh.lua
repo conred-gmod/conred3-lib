9b3ee22bf21a827725ce392f64c742aa13a33da8
stpM64 <sssstepa452@yandex.ru>
2026-07-04 00:38:34 +0300

    Added CR.Class.MakeStaticInitable

--- lua/conred/lib/class/new_delete_sh.lua
+++ lua/conred/lib/class/new_delete_sh.lua
@@ -7,10 +7,13 @@
 --- Use `CR.Class.MakeConstructable` to add me to your metatable.
 --- 
 --- @class CR.Class.Constructable: CR.Class.Base
+--- @field IsStatic false
 --- @field __init_makes_valid boolean If true, after `:Init` or `:New` is called, the object is made valid. True by-default.
 --- @field __isvalid boolean False by-default, true during and after `:Init` or `:New`.
---- @field OnInit (fun(self: CR.Class.Constructable, ...): CR.Class.Constructable)? User-defined initializer.
+--- @field OnInit (fun(self: CR.Class.Constructable, ...))? User-defined initializer.
 local CONSTR = {}
+
+CONSTR.IsStatic = false
 
 --- Construct the object without initializing it.
 --- 
@@ -18,6 +21,7 @@ local CONSTR = {}
 function CONSTR:Construct()
     local inst = {
         __isvalid = false
+        -- TODO: __index and other metatable hooks
     }
     setmetatable(inst, self)
 
@@ -69,6 +73,53 @@ function CR.Class.MakeConstructable(meta)
     meta.__isvalid = false
     meta.__init_makes_valid = true
 ---@diagnostic enable: inject-field
+end
+
+------------------------------------------------------
+
+--- A mixin that adds static initialization to static class.
+--- 
+--- Use `CR.Class.MakeStaticInitable` to add me to your metatable.
+---
+--- @class CR.Class.StaticInitable: CR.Class.Base
+--- @field IsStatic true
+--- @field __isvalid boolean False by-default, true during and after `:StaticInit` (if .__delayStaticInit == false) or `:StaticInit_Delayed`
+--- @field __delayStaticInit boolean False by-default, if true, static init will be delayed until `:StaticInit_Delayed` is called
+--- @field private __staticInitStarted boolean
+--- @field OnStaticInit (fun(self: CR.Class.StaticInitable))? Callback 
+local STINIT = {}
+
+STINIT.__isvalid = false
+STINIT.IsStatic = true
+STINIT.__delayStaticInit = false
+STINIT.__staticInitStarted = false
+
+--- Initialize static object. Call this after everything is added to the metatable.
+function STINIT:StaticInit()
+    self.__staticInitStarted = true
+
+    if not self.__delayStaticInit then
+        self:StaticInit_Delayed()
+    end
+end
+
+--- Do the delayed initialization of static object.
+--- 
+--- Do not call if delayed init (`.__delayStaticInit`) is disabled. It will error.
+function STINIT:StaticInit_Delayed()
+    assert(self.__staticInitStarted and not self.__isvalid)
+
+    if self.OnStaticInit ~= nil then
+        self:OnStaticInit()
+    end
+
+    self.__isvalid = true
+end
+
+---Adds `CR.Class.StaticInitable` mixin to `meta`
+---@param meta CR.Class.Base
+function CR.Class.MakeStaticInitable(meta)
+    table.Merge(meta, CONSTR)
 end
 
 ------------------------------------------------------
